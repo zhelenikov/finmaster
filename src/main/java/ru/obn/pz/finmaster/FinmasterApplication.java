e31d12d4277a6a5af8412e15d05b690b8d028729
Zhelenikov <zhelenikov050990@icloud.com>
2022-09-25 17:39:29 +0300

    FINMASTER-5 раскатываем пустое приложение

---
+++ src/main/java/ru/obn/pz/finmaster/FinmasterApplication.java
@@ -0,0 +1,13 @@
+package ru.obn.pz.finmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinmasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinmasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/obn/pz/finmaster/FinmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.obn.pz.finmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinmasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
