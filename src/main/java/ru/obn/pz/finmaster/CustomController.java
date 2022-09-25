0c6bd12934e28fc858414afe3c42313a3583942d
Zhelenikov <zhelenikov050990@icloud.com>
2022-09-25 22:43:03 +0300

    FINMASTER-5 для теста добавил index.html

--- src/main/java/ru/obn/pz/finmaster/CustomController.java
+++ src/main/java/ru/obn/pz/finmaster/CustomController.java
@@ -1,14 +1,14 @@
 package ru.obn.pz.finmaster;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class CustomController {
 
     @GetMapping("/")
     public String test (){
-        return "index.html";
+        return "Я работаю, не мешай!";
     }
 
 }
