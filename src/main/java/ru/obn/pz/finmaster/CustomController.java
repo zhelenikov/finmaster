ed17d48199e5b9734e7f231ec5792dfb78b2f950
Zhelenikov <zhelenikov050990@icloud.com>
2022-10-01 15:27:08 +0300

    FINMASTER-5 Procfile

--- src/main/java/ru/obn/pz/finmaster/CustomController.java
+++ src/main/java/ru/obn/pz/finmaster/CustomController.java
@@ -3,11 +3,22 @@ package ru.obn.pz.finmaster;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @RestController("/")
 public class CustomController {
+
+    private static final List<String> tickets = Stream.of("GZPR, SBER, ALRO").collect(Collectors.toList());
+
     @GetMapping
-    public String test (){
+    public String test() {
         return "Я работаю, не мешай!";
     }
 
+    @GetMapping("tickets")
+    public List<String> getAllTicket() {
+        return tickets;
+    }
 }
